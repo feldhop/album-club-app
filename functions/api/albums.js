a8f2a227bb79882550faadab73491681524a1ff1
Cory-Mandt-GVO <cmandt@govirtualoffice.com>
2023-12-20 10:20:04 -0600

    Add database

--- functions/api/albums.js
+++ functions/api/albums.js
@@ -1,3 +1,17 @@
 export const onRequestGet = function () {
-    return new Response("Looking for some albums?");
+    const stmt = DATABASE.prepare(
+        `SELECT
+            albums.id, 
+            albums.name AS albumname, 
+            artists.name AS artistname 
+        FROM albums
+            INNER JOIN artists ON artists.id = albums.artist`
+    );
+
+    return new Response(JSON.stringify(stmt.all(), null, 2),
+    {
+      headers: {
+        "content-type": "text/json"
+      }
+    });
 }
